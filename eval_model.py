f6dec44921dc364c80eb002503de24fcd74238a4
atomextranova <atomextranova@gmail.com>
2018-05-06 15:37:50 -0500

    multiple model

--- eval_model.py
+++ eval_model.py
@@ -24,6 +24,7 @@ if subtract_pixel_mean:
     x_train -= x_train_mean
     x_test -= x_train_mean
 
+
 def array_to_scalar(arr):
     list = []
     for item in arr:
@@ -39,14 +40,6 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 print('y_train shape:', y_train.shape)
-model_name = "cifar10_ResNetSVM20v3_model.028.25.0.0001"
-model = keras.models.load_model(model_name + ".h5")
-scores = model.evaluate(x_test, keras.utils.to_categorical(y_test, 10), verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
-model.summary()
-
-pred = model.predict(x_test)
 
 
 def generate_orig(name, x, pred, y):
@@ -81,7 +74,7 @@ def attack_wrapper(attack, name, gap=1, part=False):
     adv = np.array(adv, 'float32')
     print("--- " + name + " %s seconds ---" % (time.time() - start))
     print("Sucessfully generated %d images with gap %d, including %d original images" % (
-    int(adv.shape[0]), gap, count))
+        int(adv.shape[0]), gap, count))
     if part:
         name += "_part"
     elif gap != 1:
@@ -91,38 +84,45 @@ def attack_wrapper(attack, name, gap=1, part=False):
         hf.create_dataset('adv_label', data=model.predict(adv))
 
 
-# try:
-#     with h5py.File("orig.h5", "r") as hf:
-#
-# generate_orig("_" , x_test + x_train_mean, pred, y_test)
-model_adv = foolbox.models.KerasModel(model, bounds=(-1, 1), preprocessing=((0, 0, 0), 1))
+def generate_adv(model):
+    # try:
+    #     with h5py.File("orig.h5", "r") as hf:
+    #
+    # generate_orig("_" , x_test + x_train_mean, pred, y_test)
+    model_adv = foolbox.models.KerasModel(model, bounds=(-1, 1), preprocessing=((0, 0, 0), 1))
 
-attack_deep_fool_l2 = foolbox.attacks.DeepFoolL2Attack(model_adv)
+    attack_deep_fool_l2 = foolbox.attacks.DeepFoolL2Attack(model_adv)
 
-attack_DFL_INF = foolbox.attacks.DeepFoolLinfinityAttack(model_adv)
-attack_DFL_0 = foolbox.attacks.DeepFoolAttack(model_adv)
-attack_IterGradSign = foolbox.attacks.IterativeGradientSignAttack(model_adv)
+    attack_DFL_INF = foolbox.attacks.DeepFoolLinfinityAttack(model_adv)
+    attack_DFL_0 = foolbox.attacks.DeepFoolAttack(model_adv)
+    attack_IterGradSign = foolbox.attacks.IterativeGradientSignAttack(model_adv)
 
-attack_IterGrad = foolbox.attacks.IterativeGradientAttack(model_adv)
-attack_LBFGSAttack = foolbox.attacks.LBFGSAttack(model_adv)
+    attack_IterGrad = foolbox.attacks.IterativeGradientAttack(model_adv)
+    attack_LBFGSAttack = foolbox.attacks.LBFGSAttack(model_adv)
 
-attack_Local = foolbox.attacks.LocalSearchAttack(model_adv)
+    attack_Local = foolbox.attacks.LocalSearchAttack(model_adv)
 
-attack_GaussianBlur = foolbox.attacks.GaussianBlurAttack(model_adv)
+    attack_GaussianBlur = foolbox.attacks.GaussianBlurAttack(model_adv)
 
-attack_Single_Pixel = foolbox.attacks.SinglePixelAttack(model_adv)
+    attack_Single_Pixel = foolbox.attacks.SinglePixelAttack(model_adv)
 
-attack_wrapper(attack_deep_fool_l2, "DeepFool_L_2", 10)
-attack_wrapper(attack_DFL_INF, 'DeepFool_L_INF', 10)
-attack_wrapper(attack_DFL_0, "DeepFool_L_0", 10)
+    attack_wrapper(attack_deep_fool_l2, "DeepFool_L_2", 10)
+    attack_wrapper(attack_DFL_INF, 'DeepFool_L_INF', 10)
+    attack_wrapper(attack_DFL_0, "DeepFool_L_0", 10)
 
-attack_wrapper(attack_LBFGSAttack, 'LBGFS', 10)
-attack_wrapper(attack_IterGrad, "Iter_Grad", 10)
-attack_wrapper(attack_IterGradSign, "Iter_GradSign", 10)
+    attack_wrapper(attack_LBFGSAttack, 'LBGFS', 10)
+    attack_wrapper(attack_IterGrad, "Iter_Grad", 10)
+    attack_wrapper(attack_IterGradSign, "Iter_GradSign", 10)
 
-attack_wrapper(attack_Local, "Local_Search", 10)
-attack_wrapper(attack_Single_Pixel, "Single_Pixel", 10)
+    attack_wrapper(attack_Local, "Local_Search", 10)
+    attack_wrapper(attack_Single_Pixel, "Single_Pixel", 10)
 
-attack_wrapper(attack_GaussianBlur, "Gaussian_Blur", 10)
+    attack_wrapper(attack_GaussianBlur, "Gaussian_Blur", 10)
+
+
 # attack_BoundaryAttack = foolbox.attacks.BoundaryAttack(model_adv)
 # attack_wrapper(attack_BoundaryAttack, "Boundary", 10)
+model_name_list = ["cifar10_ResNetSVM20v3_model.121.0.2.L1.0.001", "cifar10_ResNetSVM20v3_model.170.0.1.L1.0.001"]
+for model_name in model_name_list:
+    model = keras.models.load_model(model_name + ".h5")
+    generate_adv(model)
