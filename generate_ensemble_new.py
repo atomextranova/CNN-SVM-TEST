a114cb53cfa0d9c9e5c081ac307228d02161eee9
atomextranova <atomextranova@gmail.com>
2018-06-22 18:49:52 -0500

    update ensemble generation

--- generate_ensemble_new.py
+++ generate_ensemble_new.py
@@ -11,44 +11,86 @@ from keras.layers import Input
 from keras.models import Model
 import h5py
 import argparse
+import itertools
 
+def generate_resnet_ensemble(subset, save_dir, input_layer, model_name):
+    child_model_list = []
+    print('Start_generating {}'.format(model_name))
+    record_name = 'result-{}.txt'.format(model_name)
+    record_path = os.path.join(save_dir, record_name)
+    file = open(record_path, 'w')
 
-def resnet_ensemble(model_locs, save_dir, image_shape):
+    file.write('Model component:\n')
+    for model_dir in subset:
+        temp_model = keras.models.load_model(model_dir)
+        temp_model.name = os.path.basename(model_dir)
+        file.write(model_dir+'\n')
+        print('Using {}'.format(model_dir))
+        # for layer in temp_model.layers:
+        #     layer.name = file + layer.name
+        # new_output = None
+        if "svm" not in model_dir:
+            # temp_model.layers.pop()
+            # temp_output = temp_model.layers[-1].output
+            # temp_model_clipped = Model(inputs=temp_model.input, outputs=temp_output, name=file)
+            # print(temp_model_clipped.summary())
+            temp_model.layers[-1].activation = keras.layers.activations.linear
+            # keras.utils.apply_modifications(temp_model)
+        new_output = temp_model(input_layer)
+        # new_output.name = file + new_output.name
+        # new_model = new_output
+        # new_model = Model(inputs=input_layer, outputs=new_output, name=file)
+        # print(new_model.summary)
+        child_model_list.append(new_output)
+    if len(child_model_list) != 1:
+        final_output = keras.layers.average(child_model_list)
+    else:
+        final_output = child_model_list[0]
+    ensemble_model = Model(inputs=input_layer, outputs=final_output, name='ensemble')
+    print(ensemble_model.summary())
+    ensemble_model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    file.write('\n')
+    print('{} done'.format(model_name))
+    file.close()
+    return ensemble_model
+
+def evaluate_model(save_dir, ensemble_model, model_name, orig_image, orig_label, mean_of_image):
+    record_name = 'result-{}.txt'.format(model_name)
+    record_path = os.path.join(save_dir, record_name)
+    file = open(record_path, 'a')
+    scores = ensemble_model.evaluate(orig_image, orig_label, verbose=0)
+    print('Model name: {}'.format(model_name))
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
+
+    file.write('Test loss: {}\n'.format(scores[0]))
+    file.write('Test accuracy: {}\n'.format(scores[1]))
+
+    file.close()
+
+def generate_resnet_ensemble_batch(model_locs, save_dir, image_shape, orig_image, orig_label, mean_of_image=[0, 0, 0], nums=[2, 3]):
+    orig_label = keras.utils.to_categorical(orig_label, 10)
     input_layer = Input(image_shape)
     model_list = []
     for root, _, files in os.walk(model_locs):
         for file in files:
             if file.endswith('h5'):
-                temp_model = keras.models.load_model(os.path.join(root, file))
-                temp_model.name = file
-                # for layer in temp_model.layers:
-                #     layer.name = file + layer.name
-                # new_output = None
-                if "svm" not in file:
-                    # temp_model.layers.pop()
-                    # temp_output = temp_model.layers[-1].output
-                    # temp_model_clipped = Model(inputs=temp_model.input, outputs=temp_output, name=file)
-                    # print(temp_model_clipped.summary())
-                    temp_model.layers[-1].activation = keras.layers.activations.linear
-                    # keras.utils.apply_modifications(temp_model)
-                    new_output = temp_model(input_layer)
-                else:
-                    new_output = temp_model(input_layer)
-                # new_output.name = file + new_output.name
-                # new_model = new_output
-                # new_model = Model(inputs=input_layer, outputs=new_output, name=file)
-                # print(new_model.summary)
-                model_list.append(new_output)
-    # output_tensors = [model.output for model in model_list]
-    # final_output = keras.;layers.average(output_tensors)
-    if len(model_list) != 1:
-        final_output = keras.layers.average(model_list)
-    else:
-        final_output = model_list[0]
-    ensemble_model = Model(inputs=input_layer, outputs=final_output, name='ensemble')
-    print(ensemble_model.summary())
-    return ensemble_model, 'Ensemble_ResNet%dv%d' % (depth, version)
+                model_list.append(os.path.join(root, file))
 
+    for model_dir in model_list:
+        model = keras.models.load_model(model_dir)
+        model_name = os.path.basename(model_dir)
+        evaluate_model(save_dir, model, model_name, orig_image, orig_label, mean_of_image)
+
+    for ensemble_num in nums:
+        for i, subset in enumerate(itertools.combinations(model_list, ensemble_num)):
+            model_name = 'ensemble_of_{}_{}'.format(ensemble_num, i)
+            ensemble_model = generate_resnet_ensemble(subset, save_dir, input_layer, model_name)
+            ensemble_dir = os.path.join(save_dir, '{}.h5'.format(model_name))
+            ensemble_model.save(ensemble_dir)
+            evaluate_model(save_dir, ensemble_model, model_name, orig_image, orig_label, mean_of_image)
 
 def generate_orig():
     if not os.path.exists('orig.h5'):
@@ -96,10 +138,10 @@ if __name__ == "__main__":
     # model_dir = sys.argv[1]
 
     parser = argparse.ArgumentParser("")
-    parser.add_argument('-m', '--model', nargs='*',
+    parser.add_argument('-m', '--model',
                         help="specify all models or model directories that is to be attacked")
     parser.add_argument('-s', '--save_dir', help="specify the save directory for attack file", default=None)
-    parser.add_argument('-g', '--gap', help='select images with gap ([::10])', type=int, default=1)
+    parser.add_argument('-g', '--gap', help='select images with gap ([::10])', type=int, default=10)
     args = parser.parse_args()
 
     model_locs = args.model
@@ -115,25 +157,7 @@ if __name__ == "__main__":
     generate_orig()
     orig_image, orig_label, mean_of_image = read_orig(gap)
     image_shape = mean_of_image.shape
-    model, model_type = resnet_ensemble(model_locs, save_dir, image_shape)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    scores = model.evaluate(orig_image, orig_label, verbose=1)
-
-    file = open('result-%s.txt' % model_type, 'w')
-    print('Test loss:', scores[0])
-    print('Test accuracy:', scores[1])
-
-    file.write('Test loss:' + str(scores[0]))
-    file.write('Test accuracy:' + str(scores[1]))
-
-    file.close()
-
-
-
-    model.save('ensemble.h5')
-
+    generate_resnet_ensemble_batch(model_locs, save_dir, image_shape, orig_image, orig_label, mean_of_image)
 # print(model.predict(x_test[::100]))
 
 #
